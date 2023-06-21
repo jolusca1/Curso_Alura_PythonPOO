ec77ca5595555884bf9748e227ec358ad3cd39cd
João Lucas <>
2023-06-21 19:00:48 -0300

    commit

--- poo avançado/modelo.py
+++ poo avançado/modelo.py
@@ -1,25 +1,83 @@
 
-class Filme:
+class Programa:
+    
+    def __init__(self, nome, ano):
+        self._nome = nome.title()
+        self.ano = ano
+        self._likes = 0
+    
+    @property
+    def nome(self):
+        return self._nome
+    
+    @nome.setter
+    def nome(self, nome):
+        self._nome = nome
+    
+    @property
+    def likes(self):
+        return self._likes
+    
+    def dar_like(self):
+        self._likes += 1
+        
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self._likes} likes'
+
+class Filme(Programa):
     
     def __init__(self, nome, ano, duracao):
-        self.nome = nome 
-        self. ano = ano
-        self.duracao = duracao
+        super().__init__(nome, ano)
+        self.duracao = duracao   
+        
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} likes'
 
-class Serie:
+
+class Serie(Programa):
     
     def __init__(self, nome, ano, temporadas):
-        self.nome = nome 
-        self. ano = ano
+        super().__init__(nome, ano)
         self.temporadas = temporadas
         
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} - {self._likes} likes'
         
-vingadores = Filme("Vingadores - Guerra Infinita", 2018, 160)
-print(vingadores.nome)
 
-atlanta = Serie("Atlanta", 2017, 2)
-print(f'''  
-      Nome: {atlanta.nome}, 
-      Ano: {atlanta.ano}, 
-      Temporadas: {atlanta.temporadas}
-      ''')
+class Playlist(list):    
+    def __init__(self, nome, programas):
+        self.nome = nome
+        super().__init__(programas)
+        
+        
+vingadores = Filme('vingadores - guerra infinita', 2018, 160)
+atlanta = Serie('atlanta', 2018, 2)
+tmep = Filme('Todo mundo em pânico', 1999, 100)
+demolidor = Serie('Demolidor', 2016, 2)
+
+vingadores.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+tmep.dar_like()
+demolidor.dar_like()
+demolidor.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+atlanta.dar_like()
+
+filmes_e_series = [
+    vingadores, 
+    atlanta,
+    demolidor,
+    tmep
+]
+
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
+    print(programa) 
+    
+print(f'Está? {demolidor in playlist_fim_de_semana}')
