1c3e830111bfd9b1d897ca55f9b1fe467a796653
João Lucas <>
2023-06-21 21:56:16 -0300

    commit

--- poo avançado/modelo.py
+++ poo avançado/modelo.py
@@ -44,10 +44,24 @@ class Serie(Programa):
         return f'{self._nome} - {self.ano} - {self.temporadas} - {self._likes} likes'
         
 
-class Playlist(list):    
+class Playlist:    
     def __init__(self, nome, programas):
         self.nome = nome
-        super().__init__(programas)
+        self._programas = programas
+    
+    def __getitem__(self, item):
+        return self._programas[item]
+    
+    def __len__(self):
+        return len(self._programas)
+    
+    @property
+    def listagem(self):
+        return self._programas
+    
+    @property 
+    def tamanho(self):
+        return len(self._programas)
         
         
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
@@ -77,7 +91,6 @@ playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
 
 print(f'Tamanho da playlist: {len(playlist_fim_de_semana)}')
 
-for programa in playlist_fim_de_semana:
+
+for programa in playlist_fim_de_semana.listagem:
     print(programa) 
-    
-print(f'Está? {demolidor in playlist_fim_de_semana}')
