9e32f1254b8ee37ab5170cdd44f9e18d2d49a29d
João Lucas <>
2023-06-23 13:50:56 -0300

    urls

---
+++ strings/projeto_url.py
@@ -0,0 +1,24 @@
+#url = "bytebank.com/cambio?moedaDestino=dolarmoedaOrigem=real"
+
+url = ""
+
+if url == "":
+    raise ValueError("A URL está vazia")
+
+indice_interrogacao = url.find('?')
+url_base = url[:indice_interrogacao]
+
+url_parametros = url[indice_interrogacao+1:]
+print(url_parametros)
+
+parametro_busca = 'moedaOrigem'
+indice_parametro = url_parametros.find(parametro_busca)
+indice_valor = indice_parametro + len(parametro_busca) + 1
+indice_e_comercial = url_parametros.find('&', indice_valor)
+
+if indice_e_comercial == -1:
+    valor = url_parametros[indice_valor:]
+else:
+    valor = url_parametros[indice_valor:indice_e_comercial]
+    
+print(valor)
